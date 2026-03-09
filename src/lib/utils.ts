5aa01c2bf569d952ff4e9e981c8f517fd7013422
CATHERINEKAMAU <catherine.kamau381@gmail.com>
2026-03-09 14:44:11 +0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,9 +5,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatCurrency(amount: number): string {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+  return 'KSh ' + new Intl.NumberFormat('en-KE', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
   }).format(amount)
 }
 
